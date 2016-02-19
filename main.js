934f00f663a78fe97e74ce00ae296e88c9fd371f
ripples <564311873@qq.com>
2016-02-19 13:40:39 +0800

    Init version.

---
+++ main.js
@@ -0,0 +1,286 @@
+var desc0 = '<p>您现在要做的是把这些卡片分成三堆，每堆数量不限。您暂时不必为每一张卡片放的地方是否准确而花时间苦苦思索。在以后的步骤里您还会有机会调动这些卡片的。</p> <p>请您仔细阅读每一张卡片。如果上面描述的行为与您孩子的行为相仿，换句话说，如果您的孩子具备这张卡片上描述的行为，您就把它放在第三组。如果卡片上所描述的行为与您孩子的行为相反，您就把这张卡片放在桌上靠左边的地方。如果卡片上所描述的行为与您孩子无很大关系或是不曾出现在他身上，您就把那张卡片放在中间的地方。这样逐条分类，很快您面前就会出现如下图所示这样三组卡片了。我们暂时把它们称作 A、B、C组。 在分类的过程中您可能会注意到，每一张卡片都描述了一种不同的行为。大多数卡片上还详细注明了与该卡片所描述的行为相反的行为表现供您参考。举例如下：</p> <pre class="editor-colors lang-text"><div class="line"><span class="text plain"><span>&nbsp;&nbsp;</span><span class="meta paragraph text"><span>13.&nbsp;当孩子因我的离去而不高兴时，他会在我走后仍持续大哭，或变得很生气。</span></span></span></div><div class="line"><span class="text plain"><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span class="meta paragraph text"><span>低分：&nbsp;我一走孩子就不再继续哭了</span></span></span></div><div class="line"><span class="text plain"><span class="meta paragraph text"><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;中间：&nbsp;孩子从不因我的离开而哭闹</span></span></span></div></pre><p>这就是说，如果上列第十三条与您孩子的行为表现相同，这张卡片就应被归入C组（右边的高分组）；若是您的孩子与该卡片下面所注的低分行为相同，则该卡片应归入Ａ组（左边的低分组）。</p> <p>您还会看到，有些卡片上除了条目以及与之相反的低分行为以外还有一句话，描述了孩子可能具备的介于高分与低分之间的行为。如上述第十三条中有如下叙述：</p> <p>这类条目通常是描述一种行为的快慢或持续时间的长短等。如果您的孩子从未出现过该行为，则这一行为的快慢与长短都没有意义了。因此，这张卡片应归于中间的Ｂ组，即无关行为组。</p> <p>  完成所有卡片的分类后，点击下一步按钮</p>';
+var desc1 = [
+    '<p>您的任务是把C组的卡片再按照表现程度分成三份， 如下所示。每一堆卡片的数目不限。但如果能分得比较均匀，则会使下一步进行得更为顺利。 左：  与你孩子的行为多少有些相似 中：  与你孩子的行为相似 右：  与你孩子的行为极为相同</p> <p>  完成所有的卡片分类后，点击下一步按钮</p>',
+    '<p>这一步您的任务是把B组的卡片再按其表现程度分成三份如下图所示。每一堆卡片的数目不限。但如果能分得比较均匀，则会使下一步进行得更为顺利。 左：  与你孩子的行为相反大于相似 中：  与你孩子的行为无关 右：  与你孩子的行为相似大于相反</p> <p>  完成所有的卡片分类后，点击下一步按钮</p>',
+    '<p>您的任务是把A组的卡片再按其表现程度分成三份如下图所示。每一堆卡片的数目不限，但如果能分得比较均匀，则会使下一步进行得更为顺利。 左：  与你孩子的行为完全相反 中：  与你孩子的行为不同 右：  与你孩子的行为不很相同</p> <p>  完成所有的卡片分类后，点击下一步按钮</p>'
+];
+var desc2 = [
+    '<p>您最后一步的分类任务是把这九堆卡片都调整为每堆不多不少整十张。</p> <p>  您先从最右边的第九堆开始调整。剩下十张您确认是您的孩子所具有的，非常典型的行为卡片，其余的卡片都归于左边的第八堆。也许您刚才分类的时候放在第九堆的卡片不足十张。如果是这样，您不妨把第八堆和第九堆的卡片混在一起，重新选出十张您认为最符合您孩子行为的卡片来，放在第九堆的位置上。您手中剩下的那些卡片便是第八堆了。</p> <p>  左侧为第八堆，右侧为第九堆</p> <p>  完成所有卡片分类后点击下一步。</p>',
+    '<p>调整第八堆卡片的数量。</p> <p>  选出十张更接近你孩子行为的卡片放置在第八堆，其余放置在第七堆。</p> <p>  左侧为第七堆，右侧为第八堆</p> <p>  完成所有卡片分类后点击下一步。</p>',
+    '<p>调整第七堆卡片的数量。</p> <p>  选出十张更接近你孩子行为的卡片放置在第七堆，其余放置在第六堆。</p> <p>  左侧为第六堆，右侧为第七堆</p> <p>  完成所有卡片分类后点击下一步。</p>',
+    '<p>调整第六堆卡片的数量。</p> <p>  选出十张更接近你孩子行为的卡片放置在第六堆，其余放置在第五堆。</p> <p>  左侧为第五堆，右侧为第六堆</p> <p>  完成所有卡片分类后点击下一步。</p>'
+];
+var desc3 = [
+    '<p>现在您从最左边的第一堆重新开始前述步骤。留下十张您认为与您孩子的行为完全相反的卡片作为第一堆，余下的归入右边的第二堆。若原有的第一堆卡片不足十张，您就把第一堆和第二堆的卡片混在一起再选出十张作为第一堆，您手中剩下的卡片则属于第二堆。依次类推，顺序完成第二堆、第三堆、和第四堆。假如一切顺利，您可完全不必调整第五堆卡片的数目。</p> <p>调整第二堆卡片的数量。</p> <p>  选出十张与你孩子行为最相反的卡片放置在第一堆，其余放置在第二堆。</p> <p>  左侧为第一堆，右侧为第二堆</p> <p>  完成所有卡片分类后点击下一步。</p>',
+    '<p>调整第二堆卡片的数量。</p> <p>  选出十张与你孩子行为最相反的卡片放置在第二堆，其余放置在第三堆。</p> <p>  左侧为第二堆，右侧为第三堆</p> <p>  完成所有卡片分类后点击下一步。</p>',
+    '<p>调整第三堆卡片的数量。</p> <p>  选出十张与你孩子行为最相反的卡片放置在第三堆，其余放置在第四堆。</p> <p>  左侧为第三堆，右侧为第四堆</p> <p>  完成所有卡片分类后点击下一步。</p>',
+    '<p>调整第四堆卡片的数量。</p> <p>  选出十张与你孩子行为最相反的卡片放置在第四堆，其余放置在第五堆。</p> <p>  左侧为第四堆，右侧为第五堆</p> <p>  完成所有卡片分类后点击下一步。</p>'
+];
+
+var problems = [{"middle": "", "low": "孩子拒绝与我分享自己的东西。", "title": "孩子能够应我的要求与我分享他/她的玩具或食物。"}, {"middle": "", "low": "孩子在玩耍中或玩完后会高高兴兴地、情绪很好地回到我的身边。", "title": "游戏后孩子回到我的身边，有时会无故烦躁。"}, {"middle": "", "low": "孩子只肯接受我的安抚。", "title": "在受伤以后或不高兴时，孩子可以接受我或其他成人的安抚。"}, {"middle": "", "low": "孩子对玩具随意乱扔，对小动物粗鲁大意。", "title": "孩子对玩具轻拿轻放，对小动物十分温柔小心。"}, {"middle": "", "low": "孩子对物比对人更感兴趣。", "title": "孩子对人比对物更感兴趣。"}, {"middle": "", "low": "孩子自己到他想去的地方去。不吵闹，也不拉扯母亲。", "title": "当孩子与我在一起时，如果看到某个他想去玩的东西，就会吵闹不休，或是想方设法把我拉到那边去。"}, {"middle": "", "low": "只有我才能轻易地逗孩子发笑，别人都不行。", "title": "孩子对谁都很爱笑。"}, {"middle": "", "low": "孩子要是哭了，也只是抽抽答答地掉眼泪，或是只大哭上一小会就过去了。", "title": "孩子一旦哭起来，就哭得很厉害。"}, {"middle": "", "low": "孩子在很多时候都是心事重重，难过伤心，或气恼不安的。", "title": "孩子大部分时间都很愉快，喜欢活动。"}, {"middle": "", "low": "听我的话，不哭也不闹。", "title": "当我中午或晚上送孩子上床睡觉时，孩子常常会哭或拒绝上床。"}, {"middle": "", "low": "孩子很少主动亲近我（除非我搂抱他或要求他亲近）。", "title": "孩子常常会不等我招呼就主动搂着我或依偎在我身旁。"}, {"middle": "孩子从来不认生，对从前未见过的人或事物也从不害怕。", "low": "孩子很认生，而且不容易适应。", "title": "孩子开始时会认生，但很快就可以陌生的人或事物熟悉起来。"}, {"middle": "孩子从不因我的离开而哭闹。", "low": "我一走孩子就不再继续哭了。", "title": "当孩子因我的离去而不高兴时，他会在我走后仍持续大哭，或变得很生气。"}, {"middle": "", "low": "孩子会静悄悄地玩他的新玩意儿，或是走到一个不会被打扰的地方去摆弄这个新东西。", "title": "孩子若是发现什么新玩意儿，他会把它拿到我这边来，或是从远远的地方举给我看。"}, {"middle": "", "low": "即使我要求，孩子也不愿意做上述事情。", "title": "孩子能够应我的要求同客人谈话，给客人看自己的玩具，或在客人面前表现自己的本事。"}, {"middle": "", "low": "孩子更喜爱球类，积木，锅碗盆勺等。", "title": "孩子喜爱以人或动物为原形的玩具，如娃娃、毛的或布的小动物等。"}, {"middle": "", "low": "尽管这个陌生人开始惹孩子不高兴了，孩子还是能够继续同他玩。", "title": "要是一个初次相识的成人把孩子惹得不高兴了，孩子很快就会对此人失去兴趣。"}, {"middle": "", "low": "孩子无视或拒绝我的建议。除非我明确地要求他时，孩子才能接受执行。", "title": "孩子乐于接受我的建议与要求。"}, {"middle": "我只有自己把东西拿走，或必须提高嗓音才能把东西要过来。", "low": "（玩笑式或游戏式的拒绝不算在内）", "title": "当我要孩子交给我某件东西时，孩子总会服从。"}, {"middle": "", "low": "轻微的碰撞、跌倒、或惊吓都会惹哭孩子。", "title": "孩子对大多数碰撞、跌倒、或惊吓都满不在乎。"}, {"middle": "我不允许孩子单独玩，或住房条件使孩子没有离开我单独玩的可能。", "low": "（如不时地叫我，能够注意到我在房间里走动或改变所做的事情等。）", "title": "孩子在家里（或班上）玩时，始终注意我的行踪。"}, {"middle": "孩子没有娃娃、小动物、或小婴儿可以玩，或他从不和这些东西/人玩。", "low": "用其他方式与这些玩具或动物玩。", "title": "孩子对玩具娃娃、小动物、或小婴儿的态度像个和善的家长一样。"}, {"middle": "", "low": "允许我与别人亲近。有时也会加入进来，但不是出于嫉妒心理。", "title": "当我对家里（或班上）的其他人表现得很亲热时，孩子会尽力设法把我的注意力吸引到自己身上来。"}, {"middle": "", "low": "孩子仅仅是因为我提高嗓音而害怕，或是因怕被惩罚而不高兴。", "title": "当我提高嗓音或严厉地对孩子讲话时，孩子会因惹母亲生气而感到难过或不好意思。"}, {"middle": "孩子从不离开我的视线独处。", "low": "孩子仍会不时地和母亲交谈或叫她。我很容易找到孩子，随时可以知道孩子在哪里、玩什么。", "title": "孩子玩耍的时候总是很安静。如果他在我视线以外的地方玩，我就很难知道他在哪里。"}, {"middle": "", "low": "无论我把孩子留给谁，他都不会哭闹。", "title": "当我要出门，把孩子留给家里的保姆、孩子的父亲或祖父母（或别的老师）时，孩子会哭闹。"}, {"middle": "我在与孩子玩耍或交谈时从不开玩笑逗他。", "low": "我逗孩子时，孩子会气恼。", "title": "我开玩笑逗孩子时，孩子会笑儿不会恼。"}, {"middle": "孩子从来就坐不住。", "low": "更愿意在地板上或某件家具上坐着。", "title": "孩子喜欢坐在我的腿上。"}, {"middle": "", "low": "即使是在全神贯注地玩，孩子也能注意到别人对他讲话。", "title": "孩子时常会因对某件事太集中注意而听不到别人对他讲话。"}, {"middle": "", "low": "孩子从不对玩具发脾气。", "title": "孩子常会轻易地对玩具发脾气。"}, {"middle": "", "low": "孩子从不在意自己是否是我的注意中心。", "title": "孩子总想成为我的注意中心。如果我很忙，或正在和别人交谈，孩子会过来打断我。"}, {"middle": "", "low": "", "title": "如果孩子做错了事，我只需说他一次，他就不再做了（至少在当时是如此），用不着我反复说。"}, {"middle": "", "low": "他要是示意我放他下来，就一定是要去玩了。", "title": "我抱着孩子时，有时孩子会示意我把他放下来（至少让我以为如此），可一旦放下他后，他又会闹着要我再把他抱起来。"}, {"middle": "孩子从不因母亲的离开而哭闹。", "low": "孩子会一个劲地追着我哭闹。", "title": "孩子不愿让我离开时会坐在原地大哭，却从不追她。"}, {"middle": "居住条件不允许，或我从不让孩子独自玩耍。", "low": "孩子喜欢在母亲身边或是和我一起玩。", "title": "孩子不缠我，喜欢独自玩耍。想去玩时，很轻松地就能离开我，从不会犹豫不决，害怕离开我。"}, {"middle": "", "low": "总是在远处玩，不叫不回来；或总是在我近旁不远处玩耍。（没有在我和他自己的游戏之间来来往往的行为）", "title": "孩子明确地表现出下列行为模式：离开我去玩一会；又回到我身边玩一会；然后再离开我玩一会。"}, {"middle": "", "low": "", "title": "孩子非常活跃，总是到处游逛。喜欢活动性强的游戏，不喜欢安静的活动。"}, {"middle": "", "low": "孩子对我很有耐心。", "title": "孩子对我总是提出各种要求，而且不耐心。如果我没有马上答应他的要求，孩子就会吵闹不休。"}, {"middle": "", "low": "孩子在独自玩耍时喜欢傻闹，往往自己笑个不停。", "title": "在远离我或独自玩耍的时候，孩子常常会显出一本正经的严肃样来。"}, {"middle": "", "low": "孩子第一次见到新东西时总是一带而过，但过一段时间以后也许还会回过头来仔细看。", "title": "孩子对新玩具或以前没有见过的东西总要仔细研究一番，或试着用不同的方法来使用或拆卸它们。"}, {"middle": "孩子常常拒绝我要他做的事。", "low": "（游戏性质的或闹着玩式的拒绝或拖延都不必算在内。）", "title": "孩子听我的话，能够及时做到我要求他做的事。"}, {"middle": "", "low": "孩子看不出我不高兴来。往往是继续玩耍，举止如常。", "title": "当我不高兴时，孩子能够觉察到，并会安静下来，自己也跟着不高兴了；或者会试着去安慰我，问我为什么不高兴。"}, {"middle": "", "low": "孩子不经常注意母亲在哪里或是在做什么。", "title": "孩子总是愿意和我在一起，或是在游戏中经常会到我身边来。"}, {"middle": "", "low": "孩子可以接受但并不十分渴望我的拥抱。有时会扭着身子要求我把他放下来。", "title": "孩子喜欢并常常要我搂着、抱着、或拥抱着他。"}, {"middle": "", "low": "孩子既不喜欢也不讨厌音乐。", "title": "孩子喜欢自己和着音乐唱歌跳舞。"}, {"middle": "", "low": "孩子整天都是磕磕绊绊的，但不见得总会受伤。", "title": "孩子四处跑或走时不会绊跟头、摔跤等。"}, {"middle": "", "low": "即使我告诉孩子这些活动是安全而有趣的，孩子还是不喜欢玩剧烈的游戏。", "title": "如果我微笑并暗示没有危险，孩子便可以接受并喜欢较大的声响或参加较剧烈的游戏。"}, {"middle": "", "low": "孩子不愿意让陌生人抱自己，也不愿意和陌生人分享自己的东西。", "title": "孩子会应初次见面的生人的要求让他抱着自己，或是与客人分享自己的东西。"}, {"middle": "有人来访时孩子从来不会跑向母亲。", "low": "见到陌生人时，孩子一开始会焦躁不安或哭着跑向母亲。（尽管过一会孩子能慢慢接近来访者）。", "title": "遇有陌生人来访时，孩子会羞怯地笑着跑到我这里来，或躲在我身后偷看。"}, {"middle": "", "low": "客人一来，孩子会主动地向客人打招呼，与他们接近。", "title": "孩子对客人最初的反应是不理睬或回避他们，尽管以后会慢慢地与客人接近。"}, {"middle": "", "low": "孩子与客人玩耍时并不喜欢和他们太亲近。", "title": "孩子和客人玩耍时喜欢在他们身上爬来爬去。"}, {"middle": "", "low": "孩子摆弄小东西或铅笔等物相当熟练。", "title": "孩子在摆弄或收拾小东西时会感到有困难。"}, {"middle": "", "low": "孩子能够让我抱他，但不会把手搭在我肩上或是搂着我。", "title": "我抱起孩子的时候，孩子会搂着我的脖子，或是把一只手搭在我肩上。"}, {"middle": "", "low": "除非我的确是在干扰他玩耍，孩子通常能够接受我的帮助。", "title": "有时我只是想帮孩子一把，孩子的反应却象是我要抢他的玩具，或是在妨碍他做自己的事情。"}, {"middle": "", "low": "孩子没有明显的模仿我的行为。", "title": "孩子会模仿我的行为或做事的方式。"}, {"middle": "", "low": "孩子认为自己可以完成很难的任务。", "title": "孩子会对看上去比较困难的活动失去兴趣，或是变得羞怯起来。"}, {"middle": "", "low": "孩子处处小心翼翼，胆怯怕事。", "title": "孩子很大胆，什么都不害怕。"}, {"middle": "", "low": "孩子对来访的客人很感兴趣，虽然开始时会有些害羞。", "title": "孩子对客人的来访从不注意，他对自己的活动更感兴趣。"}, {"middle": "", "low": "每当做完一件事或玩完一个玩具，孩子总要回到我身边来玩，或是要我帮他找到别的事情去做。", "title": "当孩子完成一项活动以后，他可以自己找到另外一件事情做而不需要去找我。"}, {"middle": "孩子从来没有害怕过什么。", "low": "即使我告诉孩子“别害怕”，孩子还是不敢去接近他害怕的东西。", "title": "如果我鼓励孩子说“没事”或是“不会伤害你的”，孩子就会接近或玩某些一开始让他感到害怕的东西。"}, {"middle": "孩子从不和我玩激烈的游戏。", "low": "孩子玩激烈的游戏时从不会伤害到我。", "title": "孩子和我玩起来的时候很野。喜欢碰撞，抓挠，咬人等（并不一定是要伤害我）。"}, {"middle": "", "low": "孩子高兴的情绪很容易变坏。", "title": "要是孩子情绪很好的话，这种好情绪往往能保持上一整天。"}, {"middle": "", "low": "孩子总是先试着去做。需要帮助时才去请别人帮助。", "title": "做事时孩子连试也不试就想找人帮着他做。"}, {"middle": "", "low": "玩耍中孩子并不特别喜欢和我有很多密切的接触。", "title": "和我玩时，孩子喜欢在母亲身上爬来爬去。"}, {"middle": "", "low": "我要求孩子变换活动内容，孩子能够接受，并不会不高兴。", "title": "如果我要孩子变换活动内容，即使我所指定的活动是孩子平时喜爱的活动，孩子还是会变得不高兴。"}, {"middle": "", "low": "孩子很不容易喜欢上初次见面的人。", "title": "孩子很容易喜欢上来访的客人，而且对人很友好。"}, {"middle": "", "low": "孩子从不试图吸引客人的注意。", "title": "家里有客人来访时，孩子总想吸引来访者的注意。"}, {"middle": "", "low": "和我比起来，孩子不如我好动。", "title": "和我比起来，孩子更为活泼好动。"}, {"middle": "孩子太小，还不会寻求帮助。", "low": "孩子经常寻求我的帮助。", "title": "孩子很少寻求我的帮助。"}, {"middle": "", "low": "除非我先打招呼，孩子从不招呼我。", "title": "见到我进门，孩子马上会显得很高兴。他会笑着向我打招呼，给我看自己的玩具，向我招手，或向我问好。"}, {"middle": "", "low": "孩子不容易被安抚好。", "title": "孩子在被惊吓或不高兴时，如果我抱着他，他就能停止哭泣，很快恢复好情绪。"}, {"middle": "", "low": "来访者的反应对孩子无所影响。", "title": "如果客人对孩子所做的事表示赞许或大笑，孩子就会一遍又一遍地重复做此事。"}, {"middle": "", "low": "孩子对这类玩具或物品无所谓，或根本没有心爱的随身物品。", "title": "孩子有自己心爱的玩具或毯子，除了要随身带在身边或陪自己睡觉外，伤心难过时也要抱着它。"}, {"middle": "", "low": "孩子能够等待适当的一段时间，似乎是在期待我满足他的要求。", "title": "要是我没有立即满足孩子的要求，孩子就会生气吵闹或是跑到一边去，好像认为我肯定不会答应他似的。"}, {"middle": "", "low": "孩子会注意到母亲的离去。他可能会跟她，但不会不高兴。", "title": "在家里，要是我走出房门，孩子就会哭闹。（可能追我，也可能不追）"}, {"middle": "", "low": "孩子喜欢与大人而不是玩具一起玩。", "title": "假如有选择余地的话，孩子更喜欢和玩具而不是和大人一起玩。"}, {"middle": "孩子还小，不可能听懂我的话。", "low": "孩子有时不能很快理解我的意思。", "title": "当我要孩子做某事时，孩子能够马上明白我的意思。（但不一定马上服从）"}, {"middle": "", "low": "孩子不喜欢让外人搂抱。", "title": "除了自己的父母、祖父母、和班上的老师之外，孩子也喜欢让别人搂着或抱着。"}, {"middle": "", "low": "除非我干扰过多，或是孩子很累，否则他是不会对我生气的。", "title": "孩子很容易生我的气。"}, {"middle": "", "low": "孩子不先观察我的表情就自行决定该怎样做。", "title": "面对看上去很危险可怕的事情，孩子会看我的脸色表情行事。"}, {"middle": "", "low": "孩子哭多半是由于真的不舒服，如疲乏、难过、害怕等。", "title": "孩子以哭为手段来要求我答应他的要求。"}, {"middle": "", "low": "孩子喜欢玩各种不同的玩具或游戏。", "title": "孩子把自己大部分的时间花在有数的几个心爱的玩具或最喜爱的活动上。"}, {"middle": "", "low": "孩子感到无聊的时候会四处闲逛或什么也不干，直到自己又找到可做的事为止。", "title": "孩子感到无聊时，会到我这里找事情做。"}, {"middle": "", "low": "孩子总是在自己身上和地上乱涂乱抹，搞得一塌糊涂。", "title": "孩子多少会注意保持家里的整洁。"}, {"middle": "", "low": "新的东西不会把孩子从自己所熟悉的玩具旁或活动中吸引开。", "title": "新的活动或玩具对孩子有极强的吸引力。"}, {"middle": "", "low": "孩子意识不到，或是不在乎我是否模仿他。", "title": "孩子会想方设法让我模仿自己。如果我主动模仿了他，他马上就能注意到并且很高兴。"}, {"middle": "", "low": "孩子不会在这方面特别受我的影响。", "title": "如果我微笑或是赞许孩子做的某件事，孩子就会一遍又一遍地重复此事。"}, {"middle": "", "low": "孩子一哭就来找我，而不等我去找他。", "title": "当某事惹得孩子不高兴时，他会待在原地大哭。"}, {"middle": "", "low": "从孩子的面部表情上别人看不出他的情绪变化。", "title": "孩子玩耍时的面部表情很强烈，也很清晰。"}, {"middle": "我不允许孩子跟随我，或是因地方太小，我不可能离开太远。", "low": "我离得远了，孩子不会跟着我走，而是自己留在原地继续玩。", "title": "如果我离开孩子正在玩的地方，孩子会主动跟着我走，在新的地方继续玩自己的游戏而不会感到不高兴。"}];
+// problems = problems.slice(0, 9);
+var cards;
+var doubleChooses;
+var tripleChooses;
+var curChooses;
+var uncategoried;
+var stage;
+var doCheck;
+var goNext;
+
+var clearCategory = function (bucketId) {
+    var subCards = cards.filter(function (card) {
+        return card.attr('bucket') == bucketId;
+    });
+    subCards.forEach(function (card) {
+        card.removeAttr('bucket');
+    });
+    uncategoried += subCards.length;
+    return subCards;
+}
+
+var goFinish = function () {
+    $('.next').hide();
+    $('.left').hide();
+    var description = $('.description');
+    description.empty().append($('<p></p>').text('分组情况如下：'));
+    for (var i = 0; i < 8; ++i) {
+        var subCards = cards.filter(function (card) {
+            return card.attr('bucket') == i;
+        });
+        var text = '第 ' + (i + 1) + ' 组：' + subCards.length + ' 张（'
+        text += subCards.map(function (card) {
+            return card.attr('index')
+        }).join(', ')
+        text +=  '）'
+        description.append($('<p></p>').text(text));
+    }
+    description.append($('<br/>'));
+    description.append($('<p></p>').text('卡片内容如下：'));
+    problems.forEach(function (problem, idx) {
+        description.append($('<p></p>').text(idx + ': ' + problem.title))
+    });
+};
+
+var goStep3 = function () {
+    console.log('step3, stage' + stage);
+    var aim = stage;
+    doCheck = function () {
+        var aimCards = cards.filter(function (card) {
+            return card.attr('bucket') == aim;
+        });
+        if (aimCards.length * 9 !== problems.length) {
+            alert('本轮左边卡片必须为' + problems.length / 9 + '张');
+            return false;
+        }
+
+        cards.forEach(function (card) {
+            if (card.attr('bucket') === undefined) {
+                card.attr('bucket', aim + 1);
+                --uncategoried;
+            }
+        });
+        return true;
+    };
+
+    $('.card').detach();
+    $('.description').html(desc3[stage]);
+
+    var subCards = clearCategory(stage).concat(clearCategory(stage + 1));
+    for (var i = stage + 2; i < 9; ++i) {
+        if (subCards.length * 9 >= problems.length) {
+            break;
+        }
+        console.log(i);
+        subCards = subCards.concat(clearCategory(i));
+    }
+    doubleChooses.empty();
+    doubleChooses.append(newChoose(stage, '左'));
+    doubleChooses.append(newChoose(stage + 1, '右'));
+    $('.bucket').remove();
+    $('.bucket_container').append(newBucket(stage, '查看左边已选卡片'));
+    $('.bucket_container').append(newBucket(stage + 1, '查看右边已选卡片'));
+    curChooses = doubleChooses;
+
+    if (++stage >= 4) {
+        goNext = goFinish;
+    }
+};
+
+var goStep2 = function () {
+    console.log('step2, stage' + stage);
+    var aim = stage;
+    doCheck = function () {
+        var aimCards = cards.filter(function (card) {
+            return card.attr('bucket') == aim;
+        });
+        if (aimCards.length * 9 !== problems.length) {
+            alert('本轮右边卡片必须为' + problems.length / 9 + '张');
+            return false;
+        }
+
+        cards.forEach(function (card) {
+            if (card.attr('bucket') === undefined) {
+                card.attr('bucket', aim - 1);
+                --uncategoried;
+            }
+        });
+        return true;
+    };
+
+    $('.card').detach();
+    $('.description').html(desc2[8 - stage]);
+
+    var subCards = clearCategory(stage - 1).concat(clearCategory(stage));
+    for (var i = stage - 2; i >= 0; --i) {
+        if (subCards.length * 9 >= problems.length) {
+            break;
+        }
+        console.log(i);
+        subCards = subCards.concat(clearCategory(i));
+    }
+    doubleChooses.empty();
+    doubleChooses.append(newChoose(stage - 1, '左'));
+    doubleChooses.append(newChoose(stage, '右'));
+    $('.bucket').remove();
+    $('.bucket_container').append(newBucket(stage - 1, '查看左边已选卡片'));
+    $('.bucket_container').append(newBucket(stage, '查看右边已选卡片'));
+    curChooses = doubleChooses;
+
+    if (--stage <= 4) {
+        stage = 0;
+        goNext = goStep3;
+    }
+};
+
+var goStep1 = function () {
+    console.log('step1, stage' + stage);
+    $('.card').detach();
+    $('.description').html(desc1[2 - stage]);
+
+    var subCards = clearCategory(stage * 3);
+    tripleChooses.empty();
+    tripleChooses.append(newChoose(stage * 3 + 1, '中'));
+    tripleChooses.append(newChoose(stage * 3, '左'));
+    tripleChooses.append(newChoose(stage * 3 + 2, '右'));
+    $('.bucket').remove();
+    $('.bucket_container').append(newBucket(stage * 3, '查看左边已选卡片'));
+    $('.bucket_container').append(newBucket(stage * 3 + 1, '查看中间已选卡片'));
+    $('.bucket_container').append(newBucket(stage * 3 + 2, '查看右边已选卡片'));
+    curChooses = tripleChooses;
+
+    if (--stage < 0) {
+        stage = 8;
+        goNext = goStep2;
+    }
+};
+
+var doChoose = function () {
+    var choice = $(this).attr('idx');
+    var card = cards[curChooses.attr('owner')];
+    if (card.attr('bucket') === choice) {
+        return;
+    }
+    if (card.attr('bucket') === undefined) {
+        --uncategoried;
+    }
+    card.attr('bucket', choice);
+    card.detach();
+};
+
+var newBucket = function (idx, text) {
+    var radio = $('<input type="radio" name="bucket">' + text + '</input>')
+        .prop('id', 'bucket' + idx)
+        .attr('index', idx)
+        .click(function () {
+            $('.card').detach();
+            var bucketId = $(this).attr('index');
+            $('.card_container').empty().append(cards.filter(function (card) {
+                return card.attr('bucket') === bucketId;
+            }));
+        });
+    return $('<label></label>')
+        .addClass('bucket')
+        .prop('for', 'bucket' + idx)
+        .append(radio);
+}
+
+var newChoose = function (idx, text) {
+    return $('<button class="choose"></button>').click(doChoose).attr('idx', idx).text(text);
+};
+
+var toggleChoose = function () {
+    curChooses.detach();
+
+    var idx = $(this).attr('index');
+    if (curChooses.attr('owner') !== idx) {
+        cards[idx].append(curChooses);
+        curChooses.attr('owner', idx);
+    } else {
+        curChooses.removeAttr('owner');
+    }
+};
+
+var newCard = function (idx) {
+    var desc = problems[idx];
+    var card = $('<div class="card"></div>')
+    var content = desc.title;
+    if (desc.middle) {
+        content += '<br>中等: ' + desc.middle;
+    }
+    if (desc.low) {
+        content += '<br>低分: ' + desc.low;
+    }
+    card.html(content);
+    card.attr('index', idx);
+    card.click(toggleChoose)
+    return card;
+};
+
+var init = function () {
+    cards = [];
+    var container = $('.card_container');
+    for (var i = 0; i < problems.length; ++i) {
+        cards[i] = newCard(i);
+    }
+    $('#uncategoried').click(function () {
+        $('.card').detach();
+        $('.card_container').append(cards.filter(function (card) {
+            return card.attr('bucket') === undefined;
+        }));
+    });
+
+    doubleChooses = $('<div class="double_chooses"></div>');
+    tripleChooses = $('<div class="triple_chooses"></div>');
+    tripleChooses.append(newChoose(3, '中'));
+    tripleChooses.append(newChoose(0, '左'));
+    tripleChooses.append(newChoose(6, '右'));
+    $('.bucket_container').append(newBucket(0, '查看左边已选卡片'));
+    $('.bucket_container').append(newBucket(3, '查看中间已选卡片'));
+    $('.bucket_container').append(newBucket(6, '查看右边已选卡片'));
+    curChooses = tripleChooses;
+    uncategoried = problems.length;
+    stage = 2;
+    doCheck = function () {
+        if (uncategoried > 0) {
+            alert('请先完成所有卡片分类');
+            return false;
+        }
+        return true;
+    }
+    goNext = goStep1;
+    $('.next').click(function () {
+        if (doCheck()) {
+            goNext();
+            $('#uncategoried').click();
+        }
+    });
+    $('#uncategoried').click();
+    $('.description').html(desc0);
+
+    $('body').keypress(function (event) {
+        if (event.keyCode === 13) {
+            $('.next').click();
+        }
+    });
+};
